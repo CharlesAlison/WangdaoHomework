8d7efb3e21bd9ace24f30f7763ceff3503e82e82
CharlesAlison <st855866@gmail.com>
2025-09-10 22:01:29 +0800

    WangdaoHomework2

---
+++ StaticList3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef int ElemType;
+typedef struct Node {
+    ElemType data;
+    struct Node *next;
+} Node, *LinkList;
+//静态链表的学习3
+int main()
+{
+    ElemType tmp;
+    scanf("%d", &tmp);
+    LinkList s, newS, head, cur, tmp2;
+    ElemType count;
+    while(tmp != 666) {
+        count++;
+        newS = (LinkList)malloc(sizeof(Node));
+        newS->data = tmp;
+        newS->next = NULL;
+        if (count == 1) {
+            head = newS;
+            s = newS;
+        }
+        else {
+            s->next = newS;
+            s = newS;
+        }
+        scanf("%d", &tmp);
+    }
+    s = head;
+    while(s) {
+        printf("%d\n", s->data);
+        s = s->next;
+    }
+    cur = head;
+    while(cur) {
+        tmp2 = cur->next;
+        free(cur);
+        cur = tmp2;
+    }
+    return 0;
+}
