f4134e9f276bfbdba82cfdb108a6401644ab422a
CharlesAlison <st855866@gmail.com>
2025-09-12 08:50:43 +0800

    WangdaoHomework2

---
+++ LinkList2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef int ElemType;
+typedef struct Node {
+    ElemType data;
+    struct Node *next;
+} Node, *LinkList;
+//动态链表的学习1
+int main()
+{
+    LinkList s = (LinkList)malloc(sizeof(Node));
+    scanf("%d", &(s->data));
+    s->next = NULL;
+    LinkList head, newS, cur, tmp;
+    head = s;
+    for (int i = 1; i <= 4; i++) {
+        newS = (LinkList)malloc(sizeof(Node));
+        scanf("%d", &(newS->data));
+        s->next = newS;
+        s = newS;
+    }
+    s = head;
+    newS->next = NULL;
+    while(s) {
+        printf("%d\n", s->data);
+        s = s->next;
+    }
+    cur = head;
+    while (cur) {
+        tmp = cur->next;
+        free(cur);
+        cur = tmp;
+    }
+
+    return 0;
+}
---
+++ LinkList4.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef int ElemType;
+typedef struct Node {
+    ElemType data;
+    struct Node *next;
+} Node, *LinkList;
+//动态链表的学习3，逆序输出
+int main()
+{
+    ElemType tmp, count;
+    scanf("%d", &tmp);
+    LinkList s, newS, head, cur, tmp2;
+    count = 0;
+    while(tmp != 666) {
+        count++;
+        newS = (LinkList)malloc(sizeof(Node));
+        newS->data = tmp;
+        newS->next = NULL;
+        if (count == 1) {
+            s = newS;
+        }
+        else {
+            newS->next = s;
+            s = newS;
+        }
+        scanf("%d", &tmp);
+    }
+    head = s;
+        while(s) {
+            printf("%d\n", s->data);
+            s = s->next;
+        }
+        cur = head;
+        while(cur) {
+            tmp2 = cur->next;
+            free(cur);
+            cur = tmp2;
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
