bfda9699b6e257338e79dc4e7c78da27a0fd38a1
CharlesAlison <st855866@gmail.com>
2025-11-21 22:01:31 +0800

    WangdaoHomework1

---
+++ addzero.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    if(n % 2 == 0) {
+        for (int i = 1; i <= n / 2; i++) {
+            a[i] = i; 
+        }
+        for (int j = n / 2 + 1; j <= n; j++) {
+            a[j] = n / 2 + (-1) * j;
+        }
+    }
+    else {
+        a[n / 2 + 1] = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            a[i] = i;
+        }
+        for (int j = (n + 1) / 2; j <= n; j++) {
+            a[j] = n / 2 + (-1) * j + 1;
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
